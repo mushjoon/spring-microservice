ebe788314ea1cb81a0292bf185d1e043caa217dc
Jack Kim <76912418+mushjoon@users.noreply.github.com>
2021-12-06 13:50:09 +0900

    Add files via upload

---
+++ ratings-data-service/src/test/java/com/jackkim/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackkim.ratingsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
