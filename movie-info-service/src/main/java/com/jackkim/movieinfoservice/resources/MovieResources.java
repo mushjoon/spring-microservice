ef1471e745a7490e6cf579cc2781c56d045cf90e
Jack Kim <76912418+mushjoon@users.noreply.github.com>
2021-12-06 13:50:37 +0900

    Add files via upload

---
+++ movie-info-service/src/main/java/com/jackkim/movieinfoservice/resources/MovieResources.java
@@ -0,0 +1,16 @@
+package com.jackkim.movieinfoservice.resources;
+
+import com.jackkim.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResources {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        return new Movie(movieId, "Titanic");
+    }
+}
